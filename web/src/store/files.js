abacb0823c167846ba4920ff10f92c684c72f11b
Marco Fontana <MaxwellJK@users.noreply.github.com>
2023-02-03 13:24:24 +0000

    Update files.js

--- web/src/store/files.js
+++ web/src/store/files.js
@@ -2,6 +2,9 @@ import { defineStore } from "pinia";
 import { useEditorStore } from "src/store/editor";
 import { useChartStore } from "src/store/chart";
 
+import { storage } from "/app/dbdiagram-oss/web/src/firebase";
+import { ref, listAll, uploadString, deleteObject } from "firebase/storage";
+
 import localforage from "localforage";
 
 const fs = localforage.createInstance({
@@ -28,10 +31,11 @@ export const useFilesStore = defineStore("files", {
     loadFileList() {
       console.log("loading file list");
 
-      fs.keys()
-        .then(keys => {
-          this.files = keys;
-        });
+      // fs.keys()
+      //   .then(keys => {
+      //     this.files = keys;
+      //   });
+      this.files = ['Tonin','Marco'];
     },
     loadFile(fileName) {
       this.loadFileList();
@@ -119,6 +123,80 @@ export const useFilesStore = defineStore("files", {
         this.currentFile = newName;
       }
       this.loadFileList();
-    }
+    },
+
+
+
+
+    listFilesGCS(){
+      console.log("loading file list from gcs");
+      
+      // Create a reference under which you want to list
+      const listRef = ref(storage, 'dbdiagram-oss/');
+      // console.log(listAll(listRef).files)
+      this.files=[];
+
+      listAll(listRef)
+      .then((res) => {
+        res.prefixes.forEach((folderRef) => {
+          // All the prefixes under listRef.
+          // You may call listAll() recursively on them.
+          
+        });
+        res.items.forEach((itemRef) => {
+          // All the items under listRef.
+          this.files.push(itemRef.name.replace('.json',''));
+        });
+      }).catch((error) => {
+        console.log("error");
+        // Uh-oh, an error occurred!
+      });
+
+      console.log(this.files);
+
+    },
+    saveFileGCS(fileName) {
+      console.log("saving file to gcs");
+
+      const editor = useEditorStore();
+      const chart = useChartStore();
+
+      const file = {
+        ...editor.save,
+        chart: chart.save
+      };
+
+      console.log(JSON.parse(JSON.stringify(file)));
+
+      const storageRef = ref(storage,'dbdiagram-oss/'+fileName+'.json');
+      uploadString(storageRef, JSON.stringify(file)).then((snapshot)=>{console.log('string uploaded')});
+      this.listFilesGCS()
+    },
+    deleteFileGCS(fileName) {
+      if (!fileName) return;
+      // fs.removeItem(fileName).then(() => {
+      //   this.loadFileList();
+      // });
+
+      // Create a reference to the file to delete
+      const file = ref(storage, 'dbdiagram-oss/'+fileName+'.json');
+
+      // Delete the file
+      deleteObject(file).then(() => {
+        // File deleted successfully
+      }).catch((error) => {
+        // Uh-oh, an error occurred!
+      });
+    },
+    renameFileGCS(newName) {
+      const oldName = this.currentFile;
+      this.saveFileGCS(newName);
+      if (oldName !== newName) {
+        // this.deleteFile(oldName);
+        this.deleteFileGCS(oldName)
+        this.currentFile = newName;
+      }
+      // this.loadFileList();
+    },
   }
 });
